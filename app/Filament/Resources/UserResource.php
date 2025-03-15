c5c83905a50ac3935ffc89f69e4c9d7e0fcaa87f
c0dy49lrra <cluerra@gmail.com>
2025-03-15 11:37:09 -0700

    update

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -34,10 +34,7 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
+                
                 Select::make('roles')
                     ->label('User Roles')
                     ->multiple()
